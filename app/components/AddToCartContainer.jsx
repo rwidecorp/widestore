9ecd76bb9f93943204e4a059972ca80fc2a181da
Trevor <trevorphilbrick@outlook.com>
2023-11-26 17:46:26 -0800

    Updates to product screen including separate components for products info and checkout things

--- app/components/AddToCartContainer.jsx
+++ app/components/AddToCartContainer.jsx
@@ -1,22 +1,50 @@
 import {
   Image,
-  Money,
   ShopPayButton,
   MediaFile,
   ModelViewer,
 } from '@shopify/hydrogen-react';
+import {CartForm} from '@shopify/hydrogen';
 
 function AddToCartContainer({selectedVariant, shop}) {
   return (
     <div className="customize-and-add-to-cart-container column">
-      <form className="customize-and-add-to-cart-form">
-        <div className="action-buttons">
-          <ShopPayButton
-            storeDomain={shop.primaryDomain.url}
-            variantIds={[selectedVariant?.id]}
-            width={'100%'}
-          />
-          <button
+      <div className="action-buttons">
+        <ShopPayButton
+          storeDomain={shop.primaryDomain.url}
+          variantIds={[selectedVariant?.id]}
+          width={'100%'}
+        />
+
+        <CartForm
+          route="/cart"
+          inputs={{
+            lines: [
+              {
+                merchandiseId: selectedVariant.id,
+              },
+            ],
+          }}
+          action={CartForm.ACTIONS.LinesAdd}
+        >
+          {(fetcher) => (
+            <>
+              <button
+                className="button-reset add-to-cart-button "
+                type="submit"
+                onClick={() => {
+                  window.location.href = window.location.href + '#cart-aside';
+                }}
+                disabled={
+                  !selectedVariant.availableForSale ?? fetcher.state !== 'idle'
+                }
+              >
+                {selectedVariant?.availableForSale ? 'Add to cart' : 'Sold out'}
+              </button>
+            </>
+          )}
+        </CartForm>
+        {/* <button
             className="button-reset add-to-cart-button "
             type="submit"
             onClick={() => {
@@ -27,9 +55,8 @@ function AddToCartContainer({selectedVariant, shop}) {
             }
           >
             {selectedVariant?.availableForSale ? 'Add to cart' : 'Sold out'}
-          </button>
-        </div>
-      </form>
+          </button> */}
+      </div>
     </div>
   );
 }
