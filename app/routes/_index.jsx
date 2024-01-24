6cabdbaccb1d325ceeeffa2e46c369593799d2d8
Trevor <trevorphilbrick@outlook.com>
2024-01-24 15:40:42 -0800

    testing deploy

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -23,7 +23,7 @@ export default function Index() {
   const [isNext, setIsNext] = useState(false);
   const [isPrevious, setIsPrevious] = useState(false);
 
-  console.log('deploy test');
+  console.log('deploy success');
 
   useEffect(() => {
     switch (selected) {
